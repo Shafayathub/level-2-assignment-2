e296da32648afeca7cebcd2df7ec3d22bf72844a
Shafayathub <shafayat.ms@gamilcom>
2023-12-13 17:17:58 +0600

    1st deploy

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -21,6 +21,21 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
+const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const result = await UserServices.getAllUsersFromDB;
+
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: [result],
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 export const Usercontrollers = {
   createUser,
+  getAllUsers,
 };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,10 @@ import { Usercontrollers } from './user.controller';
 
 const router = express.Router();
 
+// post user
 router.post('/', Usercontrollers.createUser);
+
+// get all users
+router.get('/', Usercontrollers.getAllUsers);
 
 export const UserRouters = router;
